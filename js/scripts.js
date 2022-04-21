79a65be8afb9d120aa2d731dd0f860c430004bdd
Panagiotis Spanakis <t8200158@aueb.gr>
2022-04-21 14:24:58 +0300

    Site v.1.0

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,16 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
 (function ($) {
     "use strict";
+
     $(function () {
         var dtToday = new Date();
 
