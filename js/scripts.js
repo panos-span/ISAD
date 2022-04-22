e0522f4d7df0faa11b6a8d40f22ebc11e106956c
Panagiotis Spanakis <t8200158@aueb.gr>
2022-04-22 14:08:57 +0300

    Version 1.1

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,12 @@ function myFunction() {
     }
 }
 
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function dropdown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
 (function ($) {
     "use strict";
 
